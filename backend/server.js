e110e76ebe0b7dae09639cef54a56d347cad465f
sorinpopescunr2 <96417725+sorinpopescunr2@users.noreply.github.com>
2023-04-10 16:40:22 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ function dbSyncWithRetry(maxRetries, sleepTime, db) {
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Bunnyshell - Getting Started app 230410 x8" });
+  res.json({ message: "Welcome to Bunnyshell - Getting Started app 230410 x9" });
 });
 
 require("./app/routes/bunnystart.routes")(app);
