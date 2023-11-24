0b33070d4399eee0408bd23dd4b2c4ebb7781d22
sorinpopescunr2 <96417725+sorinpopescunr2@users.noreply.github.com>
2023-11-24 11:10:26 +0200

    Update book-list.component.tsx

--- frontend/src/components/book-list.component.tsx
+++ frontend/src/components/book-list.component.tsx
@@ -138,7 +138,7 @@ export default class BookList extends Component<Props, State>{
         </div>
         ) : ( '' )}
         <div className="col-md-6">
-          <h4>Book List 231110-x1</h4>
+          <h4>Book List 231124-x1</h4>
           </div>
         <div className="col-md-6">
           <div className="input-group mb-3">
